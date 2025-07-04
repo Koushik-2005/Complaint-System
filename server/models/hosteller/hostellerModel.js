7cedb97b3f2cd6f7639b41ecd5a9b8b2eb49c8aa
RushindhraMarri <138361366+Rushindhra@users.noreply.github.com>
2025-07-04 14:00:33 +0530

    Update hostellerModel.js

--- server/models/hosteller/hostellerModel.js
+++ server/models/hosteller/hostellerModel.js
@@ -5,6 +5,10 @@ const hostellerSchema = new mongoose.Schema({
         type: String,
         required: true
     },
+    Id:{
+        type:String,
+        required:true
+    },
     firstName: { 
         type: String, 
         required: true 
@@ -31,4 +35,3 @@ const hostellerSchema = new mongoose.Schema({
 
 const Hosteller = mongoose.model('Hosteller', hostellerSchema);
 module.exports = Hosteller;
-//Wait i am stopping my screen
